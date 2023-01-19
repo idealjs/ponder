38d6673eba440d075577136f2e42643072a98c9a
cqh963852 <cqh963852@hotmail.com>
2023-01-19 21:01:01 +0800

    feat: add act test case

---
+++ packages/shared-node/src/runSchema/act.spec.ts
@@ -0,0 +1,109 @@
+import fs from "fs";
+import path from "path";
+import { describe, expect, it, vi } from "vitest";
+
+import act from "./act";
+
+describe("act", () => {
+  it("act result true", async () => {
+    let nextPayload;
+    const setNextPayload = vi.fn((_payload) => {
+      nextPayload = _payload;
+    });
+
+    let result;
+    const setResult = vi.fn((_result) => {
+      result = _result;
+    });
+
+    let error;
+    const setError = vi.fn((_error) => {
+      error = _error;
+    });
+
+    const content = fs
+      .readFileSync(path.resolve(__dirname, "./act.true.test"))
+      .toString();
+
+    const actPayload = "any payload";
+    const actResult = await act(
+      {
+        id: "testId",
+        schemaId: null,
+        url: null,
+        content,
+      },
+      {
+        payload: actPayload,
+        setNextPayload,
+        setResult,
+        setError,
+      }
+    );
+
+    expect(actResult).toBe(true);
+    expect(setNextPayload).toBeCalledTimes(1);
+    expect(setResult).toBeCalledTimes(1);
+    expect(setError).toBeCalledTimes(1);
+
+    expect(nextPayload).toEqual({
+      hello: "world",
+      inComePayload: actPayload,
+    });
+
+    expect(result).toBe("result string");
+
+    expect(error).toBe("error string");
+  });
+
+  it("act result false", async () => {
+    let nextPayload;
+    const setNextPayload = vi.fn((_payload) => {
+      nextPayload = _payload;
+    });
+
+    let result;
+    const setResult = vi.fn((_result) => {
+      result = _result;
+    });
+
+    let error;
+    const setError = vi.fn((_error) => {
+      error = _error;
+    });
+
+    const content = fs
+      .readFileSync(path.resolve(__dirname, "./act.false.test"))
+      .toString();
+
+    const actPayload = "any payload";
+    const actResult = await act(
+      {
+        id: "testId",
+        schemaId: null,
+        url: null,
+        content,
+      },
+      {
+        payload: actPayload,
+        setNextPayload,
+        setResult,
+        setError,
+      }
+    );
+
+    expect(actResult).toBe(false);
+    expect(setNextPayload).toBeCalledTimes(1);
+    expect(setResult).toBeCalledTimes(1);
+    expect(setError).toBeCalledTimes(1);
+
+    expect(nextPayload).toEqual({
+      hello: "world",
+      inComePayload: actPayload,
+    });
+
+    expect(result).toEqual({ result: "result object" });
+
+    expect(error).toEqual({ error: "error object" });
+  });
+});
