9e658bd3993b323030e513ab43a1f7404e97f319
cqh963852 <cqh963852@hotmail.com>
2023-01-30 08:38:55 +0800

    chore: update getHelper

--- packages/shared-browser/src/utils/getHelper.ts
+++ packages/shared-browser/src/utils/getHelper.ts
@@ -1,11 +1,9 @@
-const getHelper = (url: string, query: string) => {
-  console.log("test test", url);
-  return fetch(new URL(`${url}?${query}`).toString()).then(async (res) => {
+const getHelper = (url: string, query: string) =>
+  fetch(new URL(`${url}?${query}`).toString()).then(async (res) => {
     if (!res.ok) {
       throw await res.json();
     }
     return res.json();
   });
-};
 
 export default getHelper;
